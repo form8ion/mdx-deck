a9cc434b24945365ece1991ed492d9b55ace0e8e
Matt Travi <programmer@travi.org>
2023-07-15 18:24:55 -0500

    test(unit): accounted for the bundling adjustments for the cypress scaffolder

--- test/integration/features/step_definitions/common-steps.js
+++ test/integration/features/step_definitions/common-steps.js
@@ -22,7 +22,7 @@ When('the project is scaffolded', async function () {
         'mdx-deck': {
           node_modules: {
             '.pnpm': {
-              '@form8ion+cypress-scaffolder@3.0.0': {
+              '@form8ion+cypress-scaffolder@3.0.2': {
                 node_modules: {
                   '@form8ion': {
                     'cypress-scaffolder': {
